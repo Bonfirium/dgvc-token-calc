64e24e63ec76a07f69355dcf8cd5b947804b4e4e
nicklatkovich <nicklatkovich@gmail.com>
2017-12-02 16:03:03 +0300

    Input parameters

--- main_window.js
+++ main_window.js
@@ -4,9 +4,21 @@ ipcRenderer.on('onResize', (e) => setTimeout(onResize, 17))
 
 let interval = undefined
 let lastOnStepTime = undefined
+let inv = 100
+let cfp = 10
+let f4cp = 15
+let icoi_output = undefined
 
 function onLoad( ) {
     onResize( )
+    let inputs = document.getElementsByTagName('input')
+    for (let inputIndex = 0; inputIndex < inputs.length; inputIndex++) {
+        let input = inputs[inputIndex]
+        if (!input.readonly) {
+            input.onchange = onInputChanged
+        }
+    }
+    icoi_output = document.getElementById('icoi-output')
     // onStartInterval( )
 }
 
@@ -33,4 +45,15 @@ function onStep( ) {
     let now = Date.now( )
     let deltaTime = (now - lastOnStepTime) / 1000
     
+}
+
+function onInputChanged(event) {
+    let element = event.srcElement
+    let value = element.value
+    switch (element.id) {
+        case 'inv-input': inv = parseFloat(value); break
+        case 'cfp-input': cfp = parseFloat(value); break
+        case 'f4cp-input': f4cp = parseFloat(value); break
+    }
+    icoi_output.value = 100 - (cfp + f4cp)
 }
