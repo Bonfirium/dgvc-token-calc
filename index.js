8aa1371af90d51f3a20ea77246dcdeb54d9b1d44
nicklatkovich <nicklatkovich@gmail.com>
2017-12-02 16:07:25 +0300

    Renaming

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.on('ready', ( ) => {
         minHeight: 250,
     })
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
